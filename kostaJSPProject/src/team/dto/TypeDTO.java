db9f49da6db14a7e9851db340f397922c571c000
Choi, Yun jin <zzdd1558@naver.com>
2017-09-14 13:43:49 +0900

    update TypeDTO

--- kostaJSPProject/src/team/dto/TypeDTO.java
+++ kostaJSPProject/src/team/dto/TypeDTO.java
@@ -1,5 +1,42 @@
 package team.dto;
 
 public class TypeDTO {
+	
+	private int typeNum; // type_num
+	private String typeName; // type_name
+	
+	
+	// 기본 생성자
+	public TypeDTO() {
+	}
 
+	
+	// setter/getter
+
+	public TypeDTO(int typeNum, String typeName) {
+		super();
+		this.typeNum = typeNum;
+		this.typeName = typeName;
+	}
+
+
+	public int getTypeNum() {
+		return typeNum;
+	}
+
+
+	public void setTypeNum(int typeNum) {
+		this.typeNum = typeNum;
+	}
+
+
+	public String getTypeName() {
+		return typeName;
+	}
+
+
+	public void setTypeName(String typeName) {
+		this.typeName = typeName;
+	}
+	
 }
