79849624a54fcb2f4e8407c4d1a0a93388872fb5
kosta <kosta@kosta-HP>
2017-09-14 13:41:08 +0900

    CityDTO, AdminDTO 13:41

--- kostaJSPProject/src/team/dto/AdminDTO.java
+++ kostaJSPProject/src/team/dto/AdminDTO.java
@@ -1,5 +1,42 @@
 package team.dto;
 
 public class AdminDTO {
+	private int adminIdx; // admin_idx
+	private String id;
+	private String password;
+	
+	public AdminDTO() {
+	}
+
+	public AdminDTO(int adminIdx, String id, String password) {
+		super();
+		this.adminIdx = adminIdx;
+		this.id = id;
+		this.password = password;
+	}
+
+	public int getAdminIdx() {
+		return adminIdx;
+	}
+
+	public void setAdminIdx(int adminIdx) {
+		this.adminIdx = adminIdx;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
--- kostaJSPProject/src/team/dto/CityDTO.java
+++ kostaJSPProject/src/team/dto/CityDTO.java
@@ -1,5 +1,32 @@
 package team.dto;
 
 public class CityDTO {
+	private int cityNum; // city_num
+	private String cityName; // city_name
+	
+	public CityDTO() {
+	}
+
+	public CityDTO(int cityNum, String cityName) {
+		super();
+		this.cityNum = cityNum;
+		this.cityName = cityName;
+	}
+
+	public int getCityNum() {
+		return cityNum;
+	}
+
+	public void setCityNum(int cityNum) {
+		this.cityNum = cityNum;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
 
 }
